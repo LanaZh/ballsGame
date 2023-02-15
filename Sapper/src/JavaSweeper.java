85cd5e085c8e285ad790f76e5eb741fd1ff12414
ZhukovaSveta <thelumierlana@yandex.ru>
2023-02-15 22:03:57 +0300

    новое поле, добавление и проверка визуального отображени

---
+++ Sapper/src/JavaSweeper.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class JavaSweeper  extends JFrame {
+
+    public static void main (String[] arg) {
+        new JavaSweeper().setVisible(true);
+    }
+
+    public JavaSweeper () {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
